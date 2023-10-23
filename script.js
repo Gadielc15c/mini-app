1f0c598e73e3051bcfca7c60d22d4cfae41e6528
Gadielc15c <gadielcascante152001@gmail.com>
2023-10-23 13:10:38 -0400

    commit changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('actionButton');
-    button.addEventListener('click', () => {
-        alert('Botón clickeado!');
-    });
-});
+// Function to handle button clicks
+function buttonClicked(buttonText) {
+    alert("You clicked " + buttonText);
+}
+
+// Function to handle interactive text clicks
+function textClicked(textContent) {
+    alert("You clicked " + textContent);
+}
